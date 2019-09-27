3c00bfa741d637bbab8aca34c62436a3fad0ac48
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-09-28 01:06:41 +0300

    add location helper

---
+++ src/helpers/location.js
@@ -0,0 +1,12 @@
+export const parseQueryString = (string) =>
+    string.slice(1).split('&')
+        .map((queryParam) => {
+            let kvp = queryParam.split('=');
+
+            return { key: kvp[0], value: kvp[1] };
+        })
+        .reduce((query, kvp) => {
+            query[kvp.key] = kvp.value;
+
+            return query;
+        }, {});
