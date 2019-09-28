28fdf4f17a5e1cf0fdce140f71f685d899bf7b51
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-09-28 04:54:52 +0300

    add activeProject, userProjectKey, activeProjectKey api

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,17 @@
-const request = (urn, data) =>
+const requestPost = (urn, data) =>
     window.axios.post(urn, data)
         .then(({ data: { data }, status }) => ({ data, status }));
 
-export const auth = (data) => request('/auth', data);
+const requestGet = (urn) =>
+    window.axios.get(urn)
+        .then(({ data: { data }, status }) => ({ data, status }));
+
+export const auth = (data) => requestPost('/auth', data);
+
+export const activeProject = () => requestGet('/active-project');
+
+export const userProjectKey = (projectId) =>
+    requestGet(`/projects/${projectId}/user-project-key`);
+
+export const activeProjectKey = (projectId, data) =>
+    requestPost(`/projects/${projectId}/user-project-key`, data);
