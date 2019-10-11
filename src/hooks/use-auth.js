71df0c62dae05fdf39d664f82ecf5e3cd4ecc718
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-10-11 21:28:28 +0400

    add use-auth hook

---
+++ src/hooks/use-auth.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+import { auth } from 'api';
+
+export default function useAuth() {
+    const [user, setUser] = useState(null);
+
+    function signIn() {
+        auth()
+            .then(({ data }) => {
+                setUser(data);
+            })
+            .catch(e => {
+                setUser(false);
+                console.log('auth', e);
+
+                return;
+            });
+    }
+
+    useEffect(signIn, []);
+
+    return { user, signIn };
+}
