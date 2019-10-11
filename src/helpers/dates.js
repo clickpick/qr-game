bc48ac343c019f9e21f1a74165b38b8014fc7d3d
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-10-11 16:24:07 +0400

    remove moment package

--- src/helpers/dates.js
+++ src/helpers/dates.js
@@ -1,5 +1,2 @@
-import moment from 'moment';
-import 'moment/locale/ru';
-moment.locale('ru');
-
-export const getUTCOffset = () => moment().utcOffset();
+export const getTimezoneOffset = () =>
+    (new Date()).getTimezoneOffset() * -1;
