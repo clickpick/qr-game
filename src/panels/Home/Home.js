67d281312a6b2c372933dc8b2209356086a2dc28
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-10-13 19:25:57 +0400

    change Home Panel

--- src/panels/Home/Home.js
+++ src/panels/Home/Home.js
@@ -3,7 +3,6 @@ import { string, func, shape, number, array, object, bool } from 'prop-types';
 
 import connect from '@vkontakte/vk-connect';
 import { Panel } from '@vkontakte/vkui';
-import Notification from 'components/Notification';
 import Loader from 'components/Loader';
 import QRCode from 'components/QRCode';
 import Button from 'components/Button';
@@ -19,14 +18,16 @@ import './Home.css';
 
 const Home = ({
 	id,
-	user, activeProject, userProjectKey, activatedProjectKeys,
-	notificationProps,
-	scanning, qrCodeRef, shareStory,
-	openAddProjectModal,
+	user, project,
+	disabledOpenScan, qrCodeRef, shareStory,
+	openRequestFundingModal,
 	showRules
 }) => {
-	const openQR = () => connect.send('VKWebAppOpenQR');
-	const showPayForm = () =>
+	function openQR() {
+		connect.send('VKWebAppOpenQR');
+	}
+
+	function showPayForm() {
 		connect.send('VKWebAppOpenPayForm', {
 			app_id: VK.APP_ID,
 			action: 'transfer-to-user',
@@ -34,79 +35,84 @@ const Home = ({
 				user_id: VK.USER_ID
 			}
 		});
+	}
 
 	return (
 		<Panel id={id} className="Home">
-			<Notification {...notificationProps} />
-
-			<div className="Home__white-wrapper">
-				<QRCode
-					className="Home__QRCode"
-					userPic={user.avatar_200}
-					token={userProjectKey}
-					loader={<Loader />}
-					ref={qrCodeRef} />
-				<div className="Home__actions">
-					<Button
-						className="Home__action  Home__action--scan"
-						children="Сканировать QR код"
-						size="medium"
-						theme="info"
-						full
-						onClick={openQR}
-						disabled={scanning} />
-					<Button
-						className="Home__action  Home__action--share"
-						children={<Icon24ShareOutline className="Home__Icon24ShareOutline" />}
-						size="medium"
-						theme="info"
-						onClick={shareStory} />
+			{(user && project) && <>
+				<div className="Home__white-wrapper">
+					<QRCode
+						className="Home__QRCode"
+						userPic={user.avatar_200}
+						token={user.active_project_token.token}
+						loader={<Loader />}
+						ref={qrCodeRef} />
+					<div className="Home__actions">
+						<Button
+							className="Home__action  Home__action--scan"
+							children="Сканировать QR код"
+							size="medium"
+							theme="info"
+							full
+							onClick={openQR}
+							disabled={disabledOpenScan} />
+						<Button
+							className="Home__action  Home__action--share"
+							children={<Icon24ShareOutline className="Home__Icon24ShareOutline" />}
+							size="medium"
+							theme="info"
+							onClick={shareStory} />
+					</div>
 				</div>
-			</div>
 
-			<div className="Home__blue-wrapper">
-				<Wave className="Home_Wave" />
+				<div className="Home__blue-wrapper">
+					<Wave className="Home_Wave" />
 
-				<div className="Home__content">
-					<h2 className="Home__title">
-						Сканируй QR коды друзей<br />и получи приз за весь шифр
-					</h2>
+					<div className="Home__content">
+						<h2 className="Home__title">
+							Сканируй QR коды друзей<br />и получи приз за весь шифр
+						</h2>
 
-					<Cipher
-						className="Home__Cipher"
-						activatedKeys={activatedProjectKeys}
-						onClick={showRules} />
+						<Cipher
+							className="Home__Cipher"
+							activatedKeys={user.activated_project_keys}
+							onClick={showRules} />
 
-					<h2 className="Home__title">При поддержке</h2>
+						<h2 className="Home__title">При поддержке</h2>
 
-					<ProjectCard className="Home__ProjectCard" {...activeProject} onClick={showPayForm} />
+						<ProjectCard className="Home__ProjectCard" {...project} onClick={showPayForm} />
 
-					<Button
-						children="Вашему фонду нужно финансирование?"
-						size="medium"
-						theme="primary"
-						data-to="finansing"
-						full
-						onClick={openAddProjectModal} />
+						<Button
+							children="Вашему фонду нужно финансирование?"
+							size="medium"
+							theme="primary"
+							data-to="finansing"
+							full
+							onClick={openRequestFundingModal} />
+					</div>
 				</div>
-			</div>
+			</>}
 		</Panel>
 	);
 };
 
 Home.propTypes = {
 	id: string.isRequired,
-	activeProject: shape({
+	user: shape({
+		vk_user_id: number,
+		avatar_200: string,
+		active_project_token: shape({ token: string }),
+		activated_project_keys: array
+	}),
+	project: shape({
 		background: string,
 		name: string,
 		logo: string,
 		descriptiom: string,
 		raised_funds: number,
-		goal_funds: number,
+		goal_funds: number
 	}),
-	userProjectKey: string.isRequired,
-	activatedProjectKeys: array.isRequired,
-	openAddProjectModal: func,
+	openRequestFundingModal: func,
 	notificationProps: object,
 	scanning: bool,
 	qrCodeRef: object,
