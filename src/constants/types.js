984d946d83ddcca9fe090edb7600df365611b8d7
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-10-16 21:53:36 +0400

    add types constants

---
+++ src/constants/types.js
@@ -0,0 +1,7 @@
+export const FETCH_USER_LOAD = 'fetch-user-loading';
+export const FETCH_USER_SUCCESS = 'fetch-user-success';
+export const FETCH_USER_ERROR = 'fetch-user-error';
+
+export const FETCH_PROJECT_LOAD = 'fetch-project-loading';
+export const FETCH_PROJECT_SUCCESS = 'fetch-project-success';
+export const FETCH_PROJECT_ERROR = 'fetch-project-error';
