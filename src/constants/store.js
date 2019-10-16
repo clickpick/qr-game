7f432ea64788f70823b6d6769002ba2e1177cf4f
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-10-17 00:26:24 +0400

    add share store initial state in constants

--- src/constants/store.js
+++ src/constants/store.js
@@ -10,7 +10,13 @@ export const PROJECT_INITIAL_STATE = {
     data: null
 };
 
+export const SHARE_STORY_INITIAL_STATE = {
+    sharing: false,
+    error: false
+}
+
 export const INITIAL_STATE = {
     user: USER_INITIAL_STATE,
-    project: PROJECT_INITIAL_STATE
+    project: PROJECT_INITIAL_STATE,
+    shareStory: SHARE_STORY_INITIAL_STATE
 };
