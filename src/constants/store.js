550eec617293bd72c12d542cd255aec6f4191738
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-10-16 21:54:33 +0400

    add store constants

--- src/constants/store.js
+++ src/constants/store.js
@@ -1,1 +1,16 @@
-export const INITIAL_STATE = {};
+export const USER_INITIAL_STATE = {
+    error: false,
+    loading: false,
+    data: null
+};
+
+export const PROJECT_INITIAL_STATE = {
+    error: false,
+    loading: false,
+    data: null
+};
+
+export const INITIAL_STATE = {
+    user: USER_INITIAL_STATE,
+    project: PROJECT_INITIAL_STATE
+};
