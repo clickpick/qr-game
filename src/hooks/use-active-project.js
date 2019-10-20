832a0f6fb010536e2208540b55bbd3632e512a0c
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-10-20 19:41:33 +0400

    remove unusable hooks

--- src/hooks/use-active-project.js
+++
@@ -1,8 +0,0 @@
-import useRequest from 'hooks/use-request';
-import { activeProject } from 'api';
-
-export default function useActiveProject() {
-    const [{ data }, fetchProject] = useRequest(activeProject);
-
-    return [data, fetchProject];
-}
--- src/hooks/use-auth.js
+++
@@ -1,8 +0,0 @@
-import useRequest from 'hooks/use-request';
-import { auth } from 'api';
-
-export default function useAuth() {
-    const [{ data }, signIn] = useRequest(auth);
-
-    return [data, signIn];
-}
