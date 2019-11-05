60c5c8868f64a72455517a5e1d1ce5cf35929714
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-11-05 21:23:18 +0400

    add taptic notification occurred in notification-actions

--- src/actions/notification-actions.js
+++ src/actions/notification-actions.js
@@ -1,5 +1,12 @@
 import * as types from 'constants/types';
 import NOTIFICATIONS, { NOTIFICATION_DELAY } from 'constants/notifications';
+import connect from '@vkontakte/vk-connect';
+
+const notificationTypes = {
+    info: 'warning',
+    success: 'success',
+    danger: 'error',
+};
 
 const addNotification = (entities) => ({
     type: types.NOTIFICATION_ADD,
@@ -21,10 +28,16 @@ const showNotification = (notificationId, props = {}, timeout = NOTIFICATION_DEL
     }    
 
     setTimeout(() => {
-        dispatch(addNotification({
+        const notificationProps = {
             ...NOTIFICATIONS[notificationId],
             ...props
-        }));
+        };
+
+        dispatch(addNotification(notificationProps));
+
+        if (connect.supports('VKWebAppTapticNotificationOccurred')) {
+            connect.send('VKWebAppTapticNotificationOccurred', { type: notificationTypes[notificationProps.type || 'info'] });
+        }
 
         if (timeout) {
             setTimeout(() => { dispatch(closeNotification()); }, timeout);
