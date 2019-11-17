c3533cc5f2d157a3b45a92503a02be7a818d6f21
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-11-17 21:09:41 +0400

    change ThankYou Component

--- src/components/ThankYou/ThankYou.jsx
+++ src/components/ThankYou/ThankYou.jsx
@@ -8,12 +8,12 @@ import thankYou from 'images/thankyou.png';
 
 import './ThankYou.css';
 
-const ThankYou = ({ className, project }) =>
+const ThankYou = ({ className }) =>
     <div className={classNames(className, 'ThankYou')}>
         <h1 className="ThankYou__title" children="Спасибо за участие!" />
         <img className="ThankYou__image" src={thankYou} alt="Спасибо!" />
         <p className="ThankYou__message">
-            За время флэшмоба мы собрали {gaps(project.raised_funds)}₽ на проект ”{project.name}”.
+            Ты молодец!
             <br />
             Несмотря на то, что проект закончился, ты можешь пожертвовать средства, которые пойдут на этот или другие проекты этого фонда.
 		</p>
--- src/panels/Finish/Finish.js
+++ src/panels/Finish/Finish.js
@@ -21,7 +21,7 @@ const Finish = ({ id, user, project, openDonateForm, enableNotifications }) => {
     return (
         <Panel id={id} className="Finish">
             <div className="Finish__wrapper">
-                <ThankYou className="Finish__ThankYou" project={project} />
+                <ThankYou className="Finish__ThankYou" />
                 <ProjectCard
                     className="Finish__ProjectCard"
                     {...project}
