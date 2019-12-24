dd793796dc6604cd5c067246a98cfb153efef367
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-12-24 11:49:51 +0400

    add sign in story

--- src/actions/share-story-actions.js
+++ src/actions/share-story-actions.js
@@ -1,6 +1,8 @@
 import * as types from 'constants/types';
 import connect from '@vkontakte/vk-connect';
 
+import { APP_LINK } from 'constants/vk';
+
 const shareStoryLoad = () => ({
     type: types.SHARE_STORY_LOAD
 });
@@ -15,16 +17,59 @@ const fetchShareStory = (token, userPic) => async (dispatch) => {
     try {
         await connect.sendPromise('VKWebAppShowStoryBox', {
             background_type: 'none',
-            stickers: [{
-                sticker_type: 'renderable',
-                sticker: {
-                    content_type: 'image',
-                    url: `https://dev1.ezavalishin.ru/?token=${token}&userPic=${userPic}&t=${Date.now()}`,
-                    can_delete: false,
-                }
-            }]
+            stickers: [
+                {
+                    sticker_type: 'renderable',
+                    sticker: {
+                        content_type: 'image',
+                        url: `https://dev1.ezavalishin.ru/?token=${token}&userPic=${userPic}&t=${Date.now()}`,
+                        can_delete: false,
+                        transform: {
+                            translation_y: 0.08,
+                            relation_width: 0.6,
+                            gravity: 'right_top'
+                        },
+                        clickable_zones: [{
+                            action_type: 'link',
+                            action: { link: APP_LINK },
+                            clickable_area: [
+                                { x: 0, y: 0 },
+                                { x: 600, y: 0 },
+                                { x: 600, y: 600 },
+                                { x: 0, y: 600 }
+                            ]
+                        }]
+                    }
+                },
+                {
+                    sticker_type: 'renderable',
+                    sticker: {
+                        content_type: 'image',
+                        url: `https://dev1.ezavalishin.ru/sign`,
+                        can_delete: false,
+                        transform: {
+                            translation_y: -0.08,
+                            relation_width: 0.8,
+                            gravity: 'center_bottom'
+                        },
+                        clickable_zones: [{
+                            action_type: 'link',
+                            action: { link: APP_LINK },
+                            clickable_area: [
+                                { x: 0, y: 0 },
+                                { x: 1064, y: 0 },
+                                { x: 1064, y: 208 },
+                                { x: 0, y: 208 }
+                            ]
+                        }]
+                    }
+                },
+            ]
         });
-    } catch (e) {}
+    } catch (e) {
+        console.log(e);
+        
+    }
 
     dispatch(shareStorySuccess());
 }
